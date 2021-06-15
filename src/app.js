7ed0f603756b50c56a601c3eea9fcaaedda9891d
Rafael Brito <rafael.souzabrito7@gmail.com>
2021-06-15 16:53:15 -0300

    pattern setted

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import routes from './routes';
+import cors from 'cors';
 
+import routes from './routes';
 import './database';
 
 class App {
@@ -13,6 +14,14 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE'); 
+      res.header("Access-Control-Allow-Headers", 'X-PINGOTHER, Content-Type');
+      this.app.use(cors());
+      next();
+    }) 
   }
 
   routes() {
